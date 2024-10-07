364007b034920cdbb3a366c81fa9c926c1ce079f
idealflower-k <aekrcu@naver.com>
2024-10-07 10:32:14 +0900

    remove unused import

--- sanghwa/subject/phoneBook/Main.java
+++ sanghwa/subject/phoneBook/Main.java
@@ -1,11 +1,6 @@
 package study.sanghwa.subject.phoneBook;
 
-import java.io.BufferedReader;
-import java.io.FileDescriptor;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
